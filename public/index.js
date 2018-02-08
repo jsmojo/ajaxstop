bb0cf0b97785deb2fc20e96ce6599f29ad3bca3e
jsmojo <phillipjava@yahoo.com>
2018-02-07 20:48:21 -0500

    first commit

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 
-$(document).ready(function () {
+(function () {
+    callWhenRadyToGo(myCB);
+
     //Test data 
     var counter = 0;
     var streams = {
@@ -10,29 +12,57 @@ $(document).ready(function () {
         } 
     }
     function ajaxReq(obj) {
-        $.ajax({
-            url: '/data/'+obj+'.json',
-            data: obj
-        })
-        .done(function() {
-            $('#root #counter').html(++counter);
-        });
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("counter").innerHTML = ++counter;
+            }
+        };
+        xhttp.open("GET", "/data/"+obj+".json", true);
+        xhttp.send();
     }
     
     streams.each(ajaxReq);
     
     // Callback function
     function myCB() {
-        setTimeout(function() {
-            $( "#root" ).append('<div><h2>All requests complete</h2></div>' );
-        }, 500);
+        var element = document.createElement('div');
+        var title = document.createElement('h2');
+        var textnode = document.createTextNode('All requests complete'); 
+        title.appendChild(textnode); 
+        element.appendChild(title);
+        document.getElementById("root").appendChild(element );
     }
     //request to call myCB when all ajax requests are complete
-    callWhenRadyToGo(myCB);
-});
+    
+})();
 
 function callWhenRadyToGo(cb) {
-    $(document).ajaxStop(function() {
-        cb();
-    });
+    var interceptor = (function(open) {
+
+        var activeXhr = [];
+        
+        XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {
+    
+            this.addEventListener('readystatechange', function() {
+                // open has been called
+                if(this.readyState == 1 ){
+                    activeXhr.push(this);
+                }
+                // operation is complete
+                else if(this.readyState == 4 ) {
+                    var i = activeXhr.indexOf(this);
+                    if(i > -1) {
+                        activeXhr.splice(i ,1);
+                    }
+                    if(!activeXhr.length) {
+                        cb();
+                    }
+                }
+            }, false);
+    
+            open.call(this, method, url, async, user, pass);
+        };
+    
+    })(XMLHttpRequest.prototype.open);
 }
